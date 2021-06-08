946a6ac32b8993d0ada1404f9753d075a102f56c
johnspeny <johnspeny@gmail.com>
2021-06-08 23:05:28 +0300

    new adds

---
+++ Classes/GameLayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by okker on 6/6/2021.
+//
+
+#include "GameLayer.h"
---
+++ Classes/GameLayer.h
@@ -0,0 +1,15 @@
+//
+// Created by okker on 6/6/2021.
+//
+
+#ifndef TICTACTOE_GAMELAYER_H
+#define TICTACTOE_GAMELAYER_H
+
+#include "cocos2d.h"
+
+class GameLayer : public cocos2d::Scene{
+
+};
+
+
+#endif //TICTACTOE_GAMELAYER_H
